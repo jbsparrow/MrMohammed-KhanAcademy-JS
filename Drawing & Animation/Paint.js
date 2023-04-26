83a08b0567f3443c6afda7cc1907fabc485518cf
Jacob <56100195+jbsparrow@users.noreply.github.com>
2023-04-26 10:13:13 -0400

    Update Paint.js
    Add `Button` function.

--- Drawing & Animation/Paint.js
+++ Drawing & Animation/Paint.js
@@ -1,120 +1,187 @@
 // https://www.khanacademy.org/computer-programming/paint-app/5833968965697536
 
-var r = 0;
-var g = 0;
-var b = 0;
-var a = 255;
-var brushSize = 10;
-var brushType = "round";
+var r = 0
+var g = 0
+var b = 0
+var a = 255
+var brushSize = 10
+var brushType = 'round'
 
+var brushColor = color(r, g, b, a)
 
-var brushColor = color(r, g, b, a);
+var enableRedSlider = false
+var enableGreenSlider = false
+var enableBlueSlider = false
+var enableAlphaSlider = false
 
+var redSliderX = 30
+var greenSliderX = 150
+var blueSliderX = 270
+var alphaSliderY = 360
 
-var enableRedSlider = false;
-var enableGreenSlider = false;
-var enableBlueSlider = false;
-var enableAlphaSlider = false;
+var Button = function(config) {
+    this.x = config.x || 0
+    this.y = config.y || 0
+    this.width = config.width || 150
+    this.height = config.height || 50
+    this.label = config.label || 'Click'
+    this.onClick = config.onClick || function() {}
+}
 
+Button.prototype.draw = function() {
+    fill(0, 234, 255)
+    rect(this.x, this.y, this.width, this.height, 5)
+    fill(0, 0, 0)
+    textSize(19)
+    textAlign(LEFT, TOP)
+    text(this.label, this.x + 10, this.y + this.height / 4)
+}
 
-var redSliderX = 30;
-var greenSliderX = 150;
-var blueSliderX = 270;
-var alphaSliderY = 360;
+Button.prototype.isMouseInside = function() {
+    return (
+        mouseX > this.x &&
+        mouseX < this.x + this.width &&
+        mouseY > this.y &&
+        mouseY < this.y + this.height
+    )
+}
 
+Button.prototype.handleMouseClick = function() {
+    if (this.isMouseInside()) {
+        this.onClick()
+    }
+}
 
+var btn1 = new Button({
+    x: 100,
+    y: 100,
+    label: 'Please click!',
+    onClick: function() {
+        text('You made the right choice!', this.x, this.y + this.height)
+    },
+})
+btn1.draw()
+
+var btn2 = new Button({
+    x: 100,
+    y: 213,
+    label: 'No! Click ME!',
+    onClick: function() {
+        text('Yay, you picked me!', this.x, this.y + this.height)
+    },
+})
+btn2.draw()
+
+mouseClicked = function() {
+    btn1.handleMouseClick()
+    btn2.handleMouseClick()
+}
 
 var draw = function() {
-    strokeWeight(1.0);
-    stroke(0, 0, 0);
-    background(255, 255, 255);
-    fill(255, 255, 255);
-    // Colour selector
-    line(30, 375, 130, 375);
-    line(150, 375, 250, 375);
-    line(270, 375, 370, 375);
-    line(12, 360, 12, 260);
+    strokeWeight(1.0)
+    stroke(0, 0, 0)
+    background(255, 255, 255)
+    fill(255, 255, 255)
+        // Colour selector
+    line(30, 375, 130, 375)
+    line(150, 375, 250, 375)
+    line(270, 375, 370, 375)
+    line(12, 360, 12, 260)
 
-    ellipse(redSliderX, 375, 5, 5);
-    ellipse(greenSliderX, 375, 5, 5);
-    ellipse(blueSliderX, 375, 5, 5);
-    ellipse(12, alphaSliderY, 5, 5);
+    ellipse(redSliderX, 375, 5, 5)
+    ellipse(greenSliderX, 375, 5, 5)
+    ellipse(blueSliderX, 375, 5, 5)
+    ellipse(12, alphaSliderY, 5, 5)
 
-    fill(brushColor);
-    rect(6, 368, 12, 12);
-
-
+    fill(brushColor)
+    rect(6, 368, 12, 12)
 
     // Brush selector
-    if (brushType === "round") {
+    if (brushType === 'round') {
         // Selection shadow
-        noStroke();
-        fill(88, 88, 88, 100);
-        rect(347, 11, 16, 16);
-        fill(brushColor);
-        stroke(0, 0, 0);
+        noStroke()
+        fill(88, 88, 88, 100)
+        rect(347, 11, 16, 16)
+        fill(brushColor)
+        stroke(0, 0, 0)
     }
-    ellipse(355, 19, 12, 12);
-    if (brushType === "square") {
+    ellipse(355, 19, 12, 12)
+    if (brushType === 'square') {
         // Selection shadow
-        noStroke();
-        fill(88, 88, 88, 100);
-        rect(366, 12, 15, 16);
-        fill(brushColor);
-        stroke(0, 0, 0);
+        noStroke()
+        fill(88, 88, 88, 100)
+        rect(366, 12, 15, 16)
+        fill(brushColor)
+        stroke(0, 0, 0)
     }
-    rect(367, 13, 12, 12);
-
-
-};
-
+    rect(367, 13, 12, 12)
+}
 
 mousePressed = function() {
-    if (mouseX < redSliderX + 5 && mouseX > redSliderX - 5 && mouseY < 380 && mouseY > 370) {
-        enableRedSlider = true;
-    } else if (mouseX < greenSliderX + 5 && mouseX > greenSliderX - 5 && mouseY < 380 && mouseY > 370) {
-        enableGreenSlider = true;
-    } else if (mouseX < blueSliderX + 5 && mouseX > blueSliderX - 5 && mouseY < 380 && mouseY > 370) {
-        enableBlueSlider = true;
-    } else if (mouseY < alphaSliderY + 5 && mouseY > alphaSliderY - 5 && mouseX < 22 && mouseX > 2) {
-        enableAlphaSlider = true;
+    if (
+        mouseX < redSliderX + 5 &&
+        mouseX > redSliderX - 5 &&
+        mouseY < 380 &&
+        mouseY > 370
+    ) {
+        enableRedSlider = true
+    } else if (
+        mouseX < greenSliderX + 5 &&
+        mouseX > greenSliderX - 5 &&
+        mouseY < 380 &&
+        mouseY > 370
+    ) {
+        enableGreenSlider = true
+    } else if (
+        mouseX < blueSliderX + 5 &&
+        mouseX > blueSliderX - 5 &&
+        mouseY < 380 &&
+        mouseY > 370
+    ) {
+        enableBlueSlider = true
+    } else if (
+        mouseY < alphaSliderY + 5 &&
+        mouseY > alphaSliderY - 5 &&
+        mouseX < 22 &&
+        mouseX > 2
+    ) {
+        enableAlphaSlider = true
     }
-};
+}
 
 mouseDragged = function() {
     if (enableRedSlider) {
-        redSliderX = constrain(mouseX, 30, 130);
+        redSliderX = constrain(mouseX, 30, 130)
     } else if (enableGreenSlider) {
-        greenSliderX = constrain(mouseX, 150, 250);
+        greenSliderX = constrain(mouseX, 150, 250)
     } else if (enableBlueSlider) {
-        blueSliderX = constrain(mouseX, 270, 370);
+        blueSliderX = constrain(mouseX, 270, 370)
     } else if (enableAlphaSlider) {
-        alphaSliderY = constrain(mouseY, 260, 360);
+        alphaSliderY = constrain(mouseY, 260, 360)
     }
 
-    r = constrain(round((redSliderX - 30) * 2.55), 0, 255);
-    g = constrain(round((greenSliderX - 150) * 2.55), 0, 255);
-    b = constrain(round((blueSliderX - 270) * 2.55), 0, 255);
-    a = constrain(round((alphaSliderY - 260) * 2.55), 0, 255);
-    brushColor = color(r, g, b, a);
-    debug(r, g, b, a);
-};
+    r = constrain(round((redSliderX - 30) * 2.55), 0, 255)
+    g = constrain(round((greenSliderX - 150) * 2.55), 0, 255)
+    b = constrain(round((blueSliderX - 270) * 2.55), 0, 255)
+    a = constrain(round((alphaSliderY - 260) * 2.55), 0, 255)
+    brushColor = color(r, g, b, a)
+    debug(r, g, b, a)
+}
 
 mouseReleased = function() {
-    enableRedSlider = false;
-    enableGreenSlider = false;
-    enableBlueSlider = false;
-    enableAlphaSlider = false;
-};
-
+    enableRedSlider = false
+    enableGreenSlider = false
+    enableBlueSlider = false
+    enableAlphaSlider = false
+}
 
 mouseClicked = function() {
     // If clicked in square selection box
     if (mouseX < 381 && mouseX > 366 && mouseY < 26 && mouseY > 12) {
-        brushType = "square";
+        brushType = 'square'
     }
     // If clicked in circle selection box
-    if (mouseX < 355 + 6 && mouseX > 355 - 6 && mouseY < 19 + 6 && mouseY > 19 - 6) {
-        brushType = "round";
+    if (mouseX < 362 && mouseX > 347 && mouseY < 27 && mouseY > 11) {
+        brushType = 'round'
     }
-};
+}
