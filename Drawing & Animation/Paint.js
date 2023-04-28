46e6b037e0c22e8ff5fcaa6a0fb1b4367f9d3782
Jacob <56100195+jbsparrow@users.noreply.github.com>
2023-04-28 18:21:41 -0400

    Update Paint.js

--- Drawing & Animation/Paint.js
+++ Drawing & Animation/Paint.js
@@ -1,15 +1,20 @@
 // https://www.khanacademy.org/computer-programming/paint-app/5833968965697536
+frameRate(9e+18);
+
 
 var drawings = [];
 var undoneDrawings = [];
 var currentDrawing = [];
 var currentTempDrawing = [];
 var points = [];
+var undonePoints = [];
 var temporaryPoints = [];
+var buttons = [];
+var tooltips = [];
 
-var r = 0;
-var g = 0;
-var b = 0;
+var r = 150;
+var g = 209;
+var b = 99;
 var a = 255;
 var brushSize = 10;
 var brushType = "round";
@@ -31,10 +36,10 @@ var enableAlphaSlider = false;
 var enableBrushSizeSlider = false;
 
 
-var redSliderX = 30;
-var greenSliderX = 150;
-var blueSliderX = 270;
-var alphaSliderY = 360;
+var redSliderX = (r / 255) * 100 + 30; // min 30 max 130
+var greenSliderX = (g / 255) * 100 + 150; // min 150 max 250
+var blueSliderX = (b / 255) * 100 + 270; // min 270 max 370
+var alphaSliderY = (a / 255) * 100 + 260; // min 260 max 360
 var brushSizeSliderX = (brushSize * 2) + 30;
 
 var mx = 200;
@@ -91,22 +96,116 @@ var LineConstructor = function(config) {
     this.x2 = config.x2;
     this.y2 = config.y2;
     this.size = config.size || 1.0;
-    this.fillColour = config.fillColour;
-    this.stroke = config.stroke;
-    this.strokeColour = config.strokeColour || color(0, 0, 0);
+    this.colour = config.colour;
 };
 
 LineConstructor.prototype.draw = function() {
-    if (this.stroke === true) {
-        stroke(0, 0, 0);
-    } else if (this.stroke === false) {
-        stroke(this.fillColour);
-    }
     strokeWeight(this.size);
-    fill(this.fillColour);
+    stroke(this.colour);
 
     line(this.x1, this.y1, this.x2, this.y2);
 };
+
+
+var QuadConstructor = function(config) {
+    this.points = config.points;
+    this.fillColour = config.fillColour;
+    this.stroke = config.stroke;
+    this.strokeWeight = config.strokeWeight || 1.0;
+};
+
+QuadConstructor.prototype.draw = function() {
+    if (this.stroke === true) {
+        stroke(0, 0, 0);
+    } else if (this.stroke === false) {
+        noStroke();
+        if (this.points.length <= 2) {
+            stroke(this.fillColour);
+        }
+    }
+    strokeWeight(this.strokeWeight);
+    fill(this.fillColour);
+    beginShape();
+    for (var i = 0; i < this.points.length; i++) {
+        vertex(this.points[i].x, this.points[i].y);
+    }
+    endShape(CLOSE);
+};
+
+
+var Tooltip = function(config) {
+    this.x = config.x;
+    this.y = config.y;
+    this.width = config.width;
+    this.height = config.height;
+    this.text = config.text;
+    this.backgroundColour = config.backgroundColour || color(88, 88, 88, 200);
+    this.textColour = config.textColour || color(255, 255, 255);
+    this.textSize = config.textSize || 12;
+    this.textFont = config.textFont || createFont("sans-serif");
+    this.outline = config.outline;
+    this.outlineColour = config.outlineColour || color(0, 0, 0);
+    this.outlineWeight = config.outlineWeight || 1.0;
+    this.bevel = config.bevel || 0;
+    this.onHover = config.onHover || function() {};
+    this.hoverTime = config.hoverTime || 1000;
+    this.hovering = false;
+    this.hoverStart = 0;
+};
+
+Tooltip.prototype.isHovering = function() {
+    if (mouseX >= this.x && mouseX <= this.x + this.width && mouseY >= this.y && mouseY <= this.y + this.height) {
+        if (this.hovering === false) {
+            this.hoverStart = millis();
+            this.hovering = true;
+        }
+        return true;
+    } else {
+        this.hovering = false;
+        return false;
+    }
+};
+
+
+Tooltip.prototype.hoverTime = function() {
+    if (this.isHovering()) {
+        if (millis() - this.hoverStart >= this.hoverTime) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+
+Tooltip.prototype.drawTooltip = function() {
+    if (this.hoverTime()) {
+        fill(this.backgroundColour);
+        stroke(this.outlineColour);
+        strokeWeight(this.outlineWeight);
+        rect(this.x, this.y, this.width, this.height, this.bevel);
+        fill(this.textColour);
+        textFont(this.textFont);
+        textSize(this.textSize);
+        text(this.text, this.x + 5, this.y + 15);
+    }
+    fill(88, 88, 88, 150);
+    rect(this.x, this.y, this.width, this.height, this.bevel);
+};
+
+var redSliderTooltip = new Tooltip({
+    x: 10,
+    y: 10,
+    width: 100,
+    height: 20,
+    text: "Red",
+    hoverTime: 1250
+});
+tooltips.push(redSliderTooltip);
+
+
+
+
 
 var Button = function(config) {
     this.x = config.x || 50;
@@ -121,6 +220,7 @@ var Button = function(config) {
     this.strokeWeight = config.strokeWeight || 1.0;
     this.bevel = config.bevel || 0;
     this.onClick = config.onClick || function() {};
+    this.updateFill = config.updateFill || function() {};
 };
 
 Button.prototype.draw = function() {
@@ -155,7 +255,14 @@ Button.prototype.isMouseInside = function() {
 Button.prototype.handleMouseClick = function() {
     if (this.isMouseInside()) {
         this.onClick();
+        this.updateFill();
+    } else {
+        this.updateFill();
     }
+};
+
+Button.prototype.updateFill = function() {
+    this.updateFill();
 };
 
 var squareBrushData = {
@@ -170,9 +277,13 @@ var squareBrushData = {
     onClick: function() {
         brushType = "square";
         pointMode = false;
+        this.fillColour = ((brushType === "square") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
+    },
+    updateFill: function() {
+        this.fillColour = ((brushType === "square") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
     }
 };
-var squareBrushButton = new Button(squareBrushData);
+buttons.push(new Button(squareBrushData));
 
 var roundBrushData = {
     x: 347,
@@ -186,9 +297,13 @@ var roundBrushData = {
     onClick: function() {
         brushType = "round";
         pointMode = false;
+        this.fillColour = ((brushType === "round") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
+    },
+    updateFill: function() {
+        this.fillColour = ((brushType === "round") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
     }
 };
-var roundBrushButton = new Button(roundBrushData);
+buttons.push(new Button(roundBrushData));
 
 var strokeButtonData = {
     x: 276,
@@ -202,9 +317,13 @@ var strokeButtonData = {
     strokeColour: color(0, 0, 0),
     onClick: function() {
         enableStroke = !enableStroke;
+        this.fillColour = ((enableStroke === true) ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
+    },
+    updateFill: function() {
+        this.fillColour = ((enableStroke === true) ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
     }
 };
-var strokeButton = new Button(strokeButtonData);
+buttons.push(new Button(strokeButtonData));
 
 var lineButtonData = {
     x: 327,
@@ -219,9 +338,36 @@ var lineButtonData = {
     onClick: function() {
         brushType = "line";
         pointMode = true;
+        points = [];
+        this.fillColour = ((brushType === "line") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
+    },
+    updateFill: function() {
+        this.fillColour = ((brushType === "line") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
     }
 };
-var lineButton = new Button(lineButtonData);
+buttons.push(new Button(lineButtonData));
+
+var quadButtonData = {
+    x: 301,
+    y: 10,
+    width: 26,
+    height: 19,
+    fill: true,
+    stroke: false,
+    shape: 'square',
+    fillColour: ((brushType === "quad") ? color(88, 88, 88, 100) : color(255, 255, 255, 0)),
+    strokeColour: color(0, 0, 0),
+    onClick: function() {
+        brushType = "quad";
+        pointMode = true;
+        points = [];
+        this.fillColour = ((brushType === "quad") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
+    },
+    updateFill: function() {
+        this.fillColour = ((brushType === "quad") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
+    }
+};
+buttons.push(new Button(quadButtonData));
 
 
 var mouseInCanvas = function() {
@@ -232,18 +378,21 @@ var mouseInCanvas = function() {
 var draw = function() {
     strokeWeight(1.0);
     stroke(0, 0, 0);
-    background(255, 255, 255);
+    background(237, 237, 237);
 
 
     // Draw canvas
     noFill();
     strokeWeight(2.0);
+    fill(255, 255, 255);
     rect(30, 36, 360, 322);
     strokeWeight(1.0);
 
     for (var i = 0; i < drawings.length; i++) {
         // Check if drawing is of type Drawing or Array
-        if (drawings[i] instanceof Drawing || drawings[i] instanceof LineConstructor) {
+        if (drawings[i] instanceof Drawing) {
+            drawings[i].draw();
+        } else if (drawings[i] instanceof LineConstructor || drawings[i] instanceof QuadConstructor) {
             drawings[i].draw();
         } else if (drawings[i] instanceof Array) {
             for (var j = 0; j < drawings[i].length; j++) {
@@ -273,6 +422,26 @@ var draw = function() {
                 line(mx, my, mx, my);
             } else if (points.length === 1) {
                 line(points[0].x, points[0].y, mx, my);
+            }
+        } else if (brushType === "quad") {
+            // Fill extra points with mouse position
+            if (points.length === 0) {
+                stroke(brushColor);
+                strokeWeight((enableStroke === true ? brushSize : 1.0));
+                point(mx, my);
+            } else {
+                var tempPoints = [];
+                for (var i = 0; i < points.length; i++) {
+                    tempPoints.push(points[i]);
+                }
+                tempPoints.push(new PVector(mx, my));
+                var quadData = {
+                    points: tempPoints,
+                    fillColour: brushColor,
+                    stroke: ((enableStroke === true || tempPoints.length <= 2) ? true : false),
+                    strokeWeight: (tempPoints.length <= 2 ? 1.0 : brushSize)
+                };
+                new QuadConstructor(quadData).draw();
             }
         }
         strokeWeight(1.0);
@@ -314,10 +483,10 @@ var draw = function() {
 
 
     // Brush selector
-    roundBrushButton.draw();
-    squareBrushButton.draw();
-    strokeButton.draw();
-    lineButton.draw();
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].updateFill();
+        buttons[i].draw();
+    }
 
 
     stroke(0, 0, 0);
@@ -325,6 +494,7 @@ var draw = function() {
     ellipse(355, 19, 12, 12);
     rect(367, 13, 12, 12);
     line(332, 24, 340, 13); // LineConstructor drawing mode button
+    quad(304, 25, 309, 13, 319, 13, 324, 25); // Quadrilateral drawing mode button
     fill(255, 255, 255);
     rect(278, 13, 12, 12);
 };
@@ -408,20 +578,14 @@ mouseReleased = function() {
 
 
 mouseClicked = function() {
-    squareBrushButton.handleMouseClick();
-    roundBrushButton.handleMouseClick();
-    strokeButton.handleMouseClick();
-    lineButton.handleMouseClick();
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].handleMouseClick();
+    }
+    for (var i = 0; i < buttons.length; i++) {
+        buttons[i].updateFill();
+    }
 
-    squareBrushData.fillColour = ((brushType === "square") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
-    roundBrushData.fillColour = ((brushType === "round") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
-    strokeButtonData.fillColour = ((enableStroke === true) ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
-    lineButtonData.fillColour = ((brushType === "line") ? color(88, 88, 88, 100) : color(255, 255, 255, 0));
 
-    roundBrushButton = new Button(roundBrushData);
-    squareBrushButton = new Button(squareBrushData);
-    strokeButton = new Button(strokeButtonData);
-    lineButton = new Button(lineButtonData);
     if (mouseInCanvas()) {
         if (brushType === "round") {
             var drawingData = {
@@ -457,12 +621,13 @@ mouseClicked = function() {
                     x2: points[1].x,
                     y2: points[1].y,
                     size: brushSize,
-                    stroke: enableStroke,
-                    fillColour: brushColor
+                    colour: brushColor
                 };
                 drawings.push(new LineConstructor(lineData));
                 points = [];
             }
+        } else if (brushType === "quad") {
+            points.push(new PVector(mx, my));
         }
     }
 };
@@ -508,13 +673,64 @@ keyTyped = function() {
         }
     } else if (key.toString() === 'Z' || key.toString() === 'z') {
         // Undo
-        if (drawings.length > 0) {
-            undoneDrawings.push(drawings.pop());
+        if (brushType === "quad") {
+            if (points.length > 0) {
+                undonePoints.push(points.pop());
+            } else if (drawings.length > 0 && drawings[drawings.length - 1] instanceof QuadConstructor) {
+                // Undo last point
+                var poppedDrawing = drawings.pop();
+                var tempPoints = poppedDrawing.points;
+                var undonePoint = tempPoints.pop();
+                points = tempPoints;
+                undonePoints.push(undonePoint);
+                undoneDrawings.push(poppedDrawing);
+            }
+        } else if (drawings.length > 0) {
+            if (drawings.length > 0 && drawings[drawings.length - 1] instanceof QuadConstructor) {
+                // Undo last point
+                var poppedDrawing = drawings.pop();
+                var tempPoints = poppedDrawing.points;
+                var undonePoint = tempPoints.pop();
+                points = tempPoints;
+                undonePoints.push(undonePoint);
+                undoneDrawings.push(poppedDrawing);
+                brushType = "quad";
+                pointMode = true;
+            } else {
+                undoneDrawings.push(drawings.pop());
+            }
         }
     } else if (key.toString === 'X' || key.toString() === 'x') {
         // Redo
-        if (undoneDrawings.length > 0) {
+        if (brushType === "quad") {
+            if (undonePoints.length > 0) {
+                if (undoneDrawings[undoneDrawings.length - 1] instanceof QuadConstructor) {
+                    // Redo last point
+                    drawings.push(undoneDrawings.pop());
+                } else {
+                    points.push(undonePoints.pop());
+                }
+            }
+        } else if (undoneDrawings.length > 0) {
             drawings.push(undoneDrawings.pop());
         }
+    } else if (key.code === 10) {
+        if (brushType === "quad") {
+            points.push(new PVector(mx, my));
+            var quadData = {
+                points: points,
+                fillColour: brushColor,
+                stroke: ((enableStroke === true || points.length <= 2) ? true : false),
+                strokeWeight: (points.length <= 2 ? 1.0 : brushSize)
+            };
+            drawings.push(new QuadConstructor(quadData));
+            points = [];
+        }
+    }
+};
+
+mouseMoved = function() {
+    for (var i = 0; i < tooltips.length; i++) {
+        tooltips[i].drawTooltip();
     }
 };
