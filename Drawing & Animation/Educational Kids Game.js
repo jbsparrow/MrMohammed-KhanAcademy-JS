958f9ab61bd0497406322a382be68c428c16fb21
Jacob <56100195+jbsparrow@users.noreply.github.com>
2023-06-15 21:54:26 -0400

    Update Educational Kids Game.js

--- Drawing & Animation/Educational Kids Game.js
+++ Drawing & Animation/Educational Kids Game.js
@@ -7,7 +7,83 @@ var buttons = [];
 var targetShapes = [];
 var shapeBeingDragged;
 var shapeDragged = false;
-var gameState = 1;
+var gameState = 0;
+
+var nodeColor = color(40, 168, 107);
+var edgeColor = color(34, 68, 204);
+var nodeSize = 8;
+var shapes = [];
+
+var createCuboid = function(x, y, z, w, h, d) {
+    var nodes = [
+        [x, y, z],
+        [x, y, z + d],
+        [x, y + h, z],
+        [x, y + h, z + d],
+        [x + w, y, z],
+        [x + w, y, z + d],
+        [x + w, y + h, z],
+        [x + w, y + h, z + d]
+    ];
+    var edges = [
+        [0, 1],
+        [1, 3],
+        [3, 2],
+        [2, 0],
+        [4, 5],
+        [5, 7],
+        [7, 6],
+        [6, 4],
+        [0, 4],
+        [1, 5],
+        [2, 6],
+        [3, 7]
+    ];
+    return { 'nodes': nodes, 'edges': edges };
+};
+
+// Rotate shape around the z-axis
+var rotateZ3D = function(theta, nodes) {
+    var sinTheta = sin(theta);
+    var cosTheta = cos(theta);
+
+    for (var n = 0; n < nodes.length; n++) {
+        var node = nodes[n];
+        var x = node[0];
+        var y = node[1];
+        node[0] = x * cosTheta - y * sinTheta;
+        node[1] = y * cosTheta + x * sinTheta;
+    }
+};
+
+var rotateX3D = function(theta, nodes) {
+    var sinTheta = sin(theta);
+    var cosTheta = cos(theta);
+
+    for (var n = 0; n < nodes.length; n++) {
+        var node = nodes[n];
+        var y = node[1];
+        var z = node[2];
+        node[1] = y * cosTheta - z * sinTheta;
+        node[2] = z * cosTheta + y * sinTheta;
+    }
+};
+
+var rotateY3D = function(theta, nodes) {
+    var sinTheta = sin(theta);
+    var cosTheta = cos(theta);
+
+    for (var n = 0; n < nodes.length; n++) {
+        var node = nodes[n];
+        var x = node[0];
+        var z = node[2];
+        node[0] = x * cosTheta + z * sinTheta;
+        node[2] = z * cosTheta - x * sinTheta;
+    }
+};
+
+shapes.push(createCuboid(0, 0, 0, 100, 160, 50));
+shapes.push(createCuboid(120, -50, -30, 20, 100, 60));
 
 
 var mouseInShape = function(poly, mouse_X, mouse_Y) {
@@ -45,7 +121,19 @@ var mouseInShape = function(poly, mouse_X, mouse_Y) {
     return mouseintersect;
 };
 
-
+var startScreen = function() {
+    background(255, 255, 255);
+    fill(0, 0, 0);
+    textSize(50);
+    textAlign(CENTER, CENTER);
+    text("Educational Kids Game", 300, 200);
+    textSize(20);
+    text("Click to start", 300, 300);
+    noFill();
+    stroke(0, 0, 0);
+    strokeWeight(5);
+    rect(150, 350, 300, 100, 10);
+};
 
 var distanceFromShapeCenters = function(shape1, shape2, threshold) {
     var threshold = threshold || undefined;
@@ -356,6 +444,13 @@ var checkHover = function() {
 
 mouseMoved = function() {
     checkHover();
+    var dx = mouseX - pmouseX;
+    var dy = mouseY - pmouseY;
+    for (var shapeNum = 0; shapeNum < shapes.length; shapeNum++) {
+        var nodes = shapes[shapeNum].nodes;
+        rotateY3D(dx, nodes);
+        rotateX3D(dy, nodes);
+    }
 };
 
 mouseReleased = function() {
@@ -376,7 +471,35 @@ mouseReleased = function() {
 draw = function() {
     background(255, 255, 255);
     if (gameState === 0) {
-        // startScreen.draw();
+        pushMatrix();
+        translate(200, 200);
+
+        // Draw edges
+        stroke(edgeColor);
+        for (var shapeNum = 0; shapeNum < shapes.length; shapeNum++) {
+            var nodes = shapes[shapeNum].nodes;
+            var edges = shapes[shapeNum].edges;
+            for (var e = 0; e < edges.length; e++) {
+                var n0 = edges[e][0];
+                var n1 = edges[e][1];
+                var node0 = nodes[n0];
+                var node1 = nodes[n1];
+                line(node0[0], node0[1], node1[0], node1[1]);
+            }
+        }
+
+        // Draw nodes
+        fill(nodeColor);
+        noStroke();
+        for (var shapeNum = 0; shapeNum < shapes.length; shapeNum++) {
+            var nodes = shapes[shapeNum].nodes;
+            for (var n = 0; n < nodes.length; n++) {
+                var node = nodes[n];
+                // ellipse(node[0], node[1], nodeSize, nodeSize);
+            }
+        }
+        popMatrix();
+
     } else if (gameState === 1) {
         for (var i = 0; i < buttons.length; i++) {
             targetShapes[i].draw();
